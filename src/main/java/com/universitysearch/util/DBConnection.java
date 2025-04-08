13c07d846bfcd0b2fbcc20dda0f6e7a086fa12d4
Shivam Darekar <shivamdarekar123@gmail.com>
2025-04-08 14:42:43 +0530

    dbconnection push

--- src/main/java/com/universitysearch/util/DBConnection.java
+++ src/main/java/com/universitysearch/util/DBConnection.java
@@ -1,4 +1,21 @@
 package com.universitysearch.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/universitysearch";
+    private static final String USER = "root";
+    private static final String PASSWORD = "root75";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("❌ [DB ERROR] " + e.getMessage());
+            return null;
+        }
+    }
 }
